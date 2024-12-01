8d666200e465528cee1c410891ec87b103e6397f
SunStar01 <77268277+SunStar01@users.noreply.github.com>
2024-12-01 08:06:50 +0800

    2024-12-01

--- app/Http/Controllers/UserSettingController.php
+++ app/Http/Controllers/UserSettingController.php
@@ -533,7 +533,7 @@ class UserSettingController extends BaseController
         } else {
             //$this->sendStopOrder($user_num, $market, $symbol);
             //Holding 상태 주문 삭제
-            DB::table('tbl_trade_order')->where([['user_num', $user_num], ['coin_num', $coin_num], ['market', $market], ['idx', 4], ['pos_date', '']])->delete();
+            DB::table('tbl_trade_order')->where([['user_num', $user_num], ['coin_num', $coin_num], ['market', $market], ['make_price', '0'], ['profit_money', '0'], ['make_date', '']])->delete();
             //실행 상태 표기를 정지 상태로 하기
             $success = DB::table('tbl_live_coins')->where([['user_num', $user_num], ['coin_num', $coin_num], ['market', $market]])
                 ->update(
